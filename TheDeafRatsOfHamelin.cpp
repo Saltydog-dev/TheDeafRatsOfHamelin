6c1848ba8a3f679edf2e9697240eb2cabe30d64d
Stéphane Boulanger <Stphane Boulanger@LAPTOP-R5MCKF6L>
2021-08-11 12:09:24 -0400

    Ajoutez des fichiers projet.

---
+++ TheDeafRatsOfHamelin.cpp
@@ -0,0 +1,30 @@
+/**
+* \file TheDeafRatsOfHamelin.cpp
+* \author Stéphane Voulanger 
+* \brief The Pied Piper has been enlisted to play his magical tune and coax all the rats out of town.
+*        But some of the rats are deaf and are going the wrong way!
+*        How many deaf rats are there?
+* 
+*        P = The Pied Piper
+*        O~ = Rat going left
+*        ~O = Rat going right
+*        
+*        ex1 ~O~O~O~O P has 0 deaf rats
+*        ex2 P O~ O~ ~O O~ has 1 deaf rat
+*        ex3 ~O~O~O~OP~O~OO~ has 2 deaf rats
+* \version 0.1
+* \date 2021-08-11
+* \source https://www.codewars.com/kata/598106cb34e205e074000031/train/cpp
+* \copyright Copyright (c) 2021
+*/
+
+#include <iostream>
+
+
+
+int main()
+{
+	std::cout << "Hello world" << std::endl; 
+
+	return 0; 
+}
